dcbee677d49ea02dc15a4e9c7fb8010524d4cb3e
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-02-15 17:07:31 -0300

    WIP interop things

---
+++ Abstract/Abstract.InterOp/Attributes.cs
@@ -0,0 +1,18 @@
+﻿namespace Abstract.InterOp;
+
+[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Module | AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+public class AbstractNamespaceAttribute : Attribute
+{
+
+    public string? NamespaceName { get; set; } = null;
+
+    public AbstractNamespaceAttribute() {}
+    public AbstractNamespaceAttribute(string name) => NamespaceName = name;
+
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+public class AbstractStructAttribute : Attribute
+{
+
+}
