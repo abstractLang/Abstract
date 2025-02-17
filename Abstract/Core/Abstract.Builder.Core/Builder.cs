713d47f44dfd6f3d86c7f7e0349dd30d7e850381
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-02-17 19:37:55 -0300

    WIP random things and building system

---
+++ Abstract/Core/Abstract.Builder.Core/Builder.cs
@@ -0,0 +1,44 @@
+﻿using Abstract.InterOp;
+
+namespace Abstract.Builder.Core;
+
+/// <summary>
+/// Build namespace.
+/// Countains interoperable structures to help
+/// building abstract solutions inside abstract
+/// code.
+/// </summary>
+[AbstractNamespace("Build")]
+public static class BuildNamespace
+{
+    /// <summary>
+    /// The base for a build instance
+    /// </summary>
+    [AbstractStruct]
+    public abstract class Builder
+    {
+
+
+
+    }
+
+    /// <summary>
+    /// A abstract build step
+    /// </summary>
+    [AbstractStruct]
+    public abstract class Step
+    {
+
+    }
+
+
+    /// <summary>
+    /// Reference to a generated executable
+    /// </summary>
+    [AbstractStruct]
+    public class Executable
+    {
+
+    }
+
+}
