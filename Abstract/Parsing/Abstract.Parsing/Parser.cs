88840fd70a9acc31dbaa877114ef0dcd66da5239
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-02-18 23:17:21 -0300

    commiting because uhhh green squares

---
+++ Abstract/Parsing/Abstract.Parsing/Parser.cs
@@ -0,0 +1,13 @@
+﻿using Abstract.Parsing.Core;
+
+namespace Abstract.Parsing;
+
+public static class Parser
+{
+
+    public SyntaxTree BuildTree(Token[] tokens)
+    {
+
+    }
+
+}
---
+++ Abstract/Res/Abstract.Resources/DefaultBuild.cs
@@ -0,0 +1,15 @@
+﻿using Abstract.Builder.Core;
+
+namespace Abstract.Resources;
+
+public class DefaultBuild
+{
+
+    public void Build(BuildNamespace.Builder b)
+    {
+
+        Console.WriteLine("Fuuuuck someone fucking help me");
+
+    }
+
+}
