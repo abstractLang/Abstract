6f7c38ad65a43350a11cd887a90cefc923b8a766
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-06-16 16:02:12 -0300

    Analizer: Add parsing to function overloads and fields

--- Compiler/Parsing/Analizer.cs
+++ Compiler/Parsing/Analizer.cs
@@ -37,40 +37,61 @@ public class Analizer
         var newPiece = new Piece(member_name, tree);
         _pieces.Add(newPiece);
 
-        ParseNodeRecursive(newPiece, member, tree.root);
+        ParseScopeRecursive(newPiece, member, tree.root.Children);
     }
 
-    private void ParseNodeRecursive(Piece piece, ProgramMember? parent, ISyntaxNode node)
+    private void ParseScopeRecursive(Piece piece, ProgramMember? parent, ISyntaxNode[] scope)
     {
+        List<Attribute> attributes_on_hold = [];
         SyntaxNode snode;
 
-        switch (node.Kind)
+        foreach (var i in scope)
         {
-            case NodeKind.Root:
-                snode = (SyntaxNode)node;
-                foreach (var i in snode.Children) ParseNodeRecursive(piece, parent, i);
-                break;
+            switch (i.Kind)
+            {
+                case NodeKind.FromImport:
+                    Console.WriteLine(i);
+                    break;
 
-            case NodeKind.FromImport:
-                Console.WriteLine(node);
-                break;
 
-            case NodeKind.FunctionDeclaration:
-                snode = (SyntaxNode)node;
-                ParseFunction(piece, parent, snode);
-                break;
+                case NodeKind.Attribute:
+                    snode = (SyntaxNode)i;
+                    attributes_on_hold.Add(ParseAttribute(piece, parent, snode));
+                    break;
 
-            case NodeKind.StructureDeclaration:
-                snode = (SyntaxNode)node;
-                ParseStructure(piece, parent, snode);
-                break;
 
-            default:
-                Console.WriteLine($"AST node not being analized: {node.Kind}");
-                break;
+                case NodeKind.FunctionDeclaration:
+                    snode = (SyntaxNode)i;
+                    var spmf = ParseFunction(piece, parent, snode);
+                    spmf.attributes.AddRange(attributes_on_hold);
+                    attributes_on_hold.Clear();
+                    break;
+
+                case NodeKind.StructureDeclaration:
+                    snode = (SyntaxNode)i;
+                    var pms = ParseStructure(piece, parent, snode);
+                    pms.attributes.AddRange(attributes_on_hold);
+                    attributes_on_hold.Clear();
+                    break;
+
+                case NodeKind.Variable:
+                    snode = (SyntaxNode)i;
+                    var pmf = ParseField(piece, parent, snode);
+                    pmf.attributes.AddRange(attributes_on_hold);
+                    attributes_on_hold.Clear();
+                    break;
+
+
+                default:
+                    Console.WriteLine($"AST node not being analized: {i.Kind}\n{i}");
+                    break;
+            }
         }
+
+        if (attributes_on_hold.Count > 0) throw new Exception("Unused attributes!");
     }
-    private void ParseFunction(Piece piece, ProgramMember? parent, SyntaxNode function)
+
+    private SubProgramMember_Function ParseFunction(Piece piece, ProgramMember? parent, SyntaxNode function)
     {
         var id = GetIdentifierAsStringArray((SyntaxNode)function.Children[1])[0];
         Console.WriteLine($"function: \"{id}\"");
@@ -86,9 +107,12 @@ public class Analizer
         if (pmember is not ProgramMember_FunctionGroup @fgroup)
             throw new Exception($"identifier {string.Join('.', name)} already used by {pmember.GetType().Name}");
 
-        fgroup.functions.Add(new());
+        var f = new SubProgramMember_Function();
+        fgroup.functions.Add(f);
+
+        return f;
     }
-    private void ParseStructure(Piece piece, ProgramMember? parent, SyntaxNode structure)
+    private ProgramMember_Structure ParseStructure(Piece piece, ProgramMember? parent, SyntaxNode structure)
     {
         var id = GetIdentifierAsStringArray((SyntaxNode)structure.Children[1])[0];
         Console.WriteLine($"structure: \"{id}\"");
@@ -101,6 +125,48 @@ public class Analizer
             _nametable.Add(name, pmember);
         }
         else throw new Exception($"identifier {string.Join('.', name)} already used by {pmember.GetType().Name}");
+
+        // Parse inner scope
+        ParseScopeRecursive(piece, pmember, ((SyntaxNode)structure.Children[2]).Children[1..^1]);
+
+        return (ProgramMember_Structure)pmember;
+    }
+    private ProgramMember_Field ParseField(Piece piece, ProgramMember? parent, SyntaxNode field)
+    {
+        string id;
+        
+        if (field.Children.Length == 2) id = GetIdentifierAsStringArray((SyntaxNode)field.Children[1])[0];
+        else if (field.Children.Length > 2) id = GetIdentifierAsStringArray((SyntaxNode)field.Children[2])[0];
+        else throw new NotImplementedException();
+
+        Console.WriteLine($"field: \"{id}\"");
+
+        string[] name = [.. parent?.Fully_qualified_namespace ?? [], id];
+
+        if (!_nametable.TryGetValue(name, out var pmember))
+        {
+            pmember = new ProgramMember_Field(parent?.member_namespace, id);
+            _nametable.Add(name, pmember);
+        }
+        else throw new Exception($"identifier {string.Join('.', name)} already used by {pmember.GetType().Name}");
+
+        return (ProgramMember_Field)pmember;
+    }
+
+    private Attribute ParseAttribute(Piece piece, ProgramMember? parent, SyntaxNode attrib)
+    {
+        var newattrb = new Attribute();
+        newattrb.identifier = GetIdentifierAsStringArray(attrib.Children[1]);
+        if (attrib.Children.Length > 3)
+        {
+            List<ISyntaxNode> args = [];
+            for (var i = 2; i < attrib.Children.Length - 1; i++)
+            {
+                args.Add(attrib.Children[i]);
+            }
+            newattrb.args = [.. args];
+        }
+        return newattrb;
     }
 
     private string[] GetIdentifierAsStringArray(ISyntaxNode identifier)
@@ -122,7 +188,18 @@ public class Analizer
         Console.WriteLine("Nametable:");
         foreach (var i in _nametable)
         {
-            Console.WriteLine($"- {PMNamespace.Combine(i.Key),-50}: {i.Value.GetType().Name}");
+
+            Console.WriteLine($"- {PMNamespace.Combine(i.Key),-35}: {i.Value.GetType().Name,-38} - {string.Join(", ", i.Value.attributes.Select(e => PMNamespace.Combine(e.identifier)))}");
+
+            if (i.Value is ProgramMember_FunctionGroup @funcg)
+            {
+                for (var j = 0; j < funcg.functions.Count; j++)
+                {
+                    var f = funcg.functions[j];
+                    Console.WriteLine($"\t- {j,-28}: {f.GetType().Name,-38} - {string.Join(", ", f.attributes.Select(e => PMNamespace.Combine(e.identifier)))}");
+                }
+            }
+
         }
 
     }
@@ -139,6 +216,7 @@ public class Analizer
     // Program members
     abstract class ProgramMember(string[]? member_namespace, string identifier)
     {
+        public List<Attribute> attributes = [];
         public readonly string[] member_namespace = member_namespace ?? [];
         public readonly string identifier = identifier;
         public string[] Fully_qualified_namespace => [.. member_namespace, identifier];
@@ -150,17 +228,20 @@ public class Analizer
     }
     class ProgramMember_Structure(string[]? member_namespace, string identifier) : ProgramMember(member_namespace, identifier)
     {
-
+        public List<ProgramMember_Field> instanceFields = [];
     }
     class ProgramMember_FunctionGroup(string[]? member_namespace, string identifier) : ProgramMember(member_namespace, identifier)
     {
         public List<SubProgramMember_Function> functions = [];
     }
+    class ProgramMember_Field(string[]? member_namespace, string identifier) : ProgramMember(member_namespace, identifier)
+    {
+        
+    }
 
     // Unispecific
-    class SubProgramMember_Function()
+    class SubProgramMember_Function() : ProgramMember(["subfunc"], "")
     {
-
     }
 
     class StringListComparer : IEqualityComparer<string[]>
@@ -179,5 +260,9 @@ public class Analizer
         }
     }
 
-
+    class Attribute
+    {
+        public string[] identifier = null!;
+        public ISyntaxNode[] args = [];
+    }
 }
